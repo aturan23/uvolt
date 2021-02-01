b97fe0d143e1aa42f92a51f7df8623f896f69b0b
Turan Assylkhan <turan@strongte.am>
2021-02-01 14:04:43 +0600

    feat: for tableView & collectionView

---
+++ uvolt/Source/UIKit/Common/ReusableView.swift
@@ -0,0 +1,63 @@
+//
+//  ReusableView.swift
+//  uvolt
+//
+//  Created by Turan Assylkhan on 01.02.2021.
+//  Copyright © 2021 Turan Assylkhan. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - TableView and Cell Extensions for convenient Cell registration
+protocol ReusableView: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return NSStringFromClass(self)
+    }
+}
+
+extension UICollectionViewCell: ReusableView { }
+extension UITableViewCell: ReusableView { }
+extension UITableViewHeaderFooterView: ReusableView { }
+
+extension UITableView {
+    func register<T: UITableViewCell>(_: T.Type) {
+        register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        
+        guard let cell = dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        
+        return cell
+    }
+    
+    func register<T: UITableViewHeaderFooterView>(_: T.Type) {
+        register(T.self, forHeaderFooterViewReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func dequeReusableHeaderView<T: UITableViewHeaderFooterView>() -> T {
+        guard let view = dequeueReusableHeaderFooterView(withIdentifier: T.defaultReuseIdentifier) as? T else {
+            fatalError("Could not dequeue headerView with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return view
+    }
+}
+
+extension UICollectionView {
+    func register<T: UICollectionViewCell>(_: T.Type) {
+        register(T.self, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func dequeReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return cell
+    }
+}
