450711004472b3c584a9a5db0bb49cf70568407c
Turan Assylkhan <turan@strongte.am>
2021-02-01 07:51:41 +0600

    Initial Commit

---
+++ uvolt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uvolt
+//
+//  Created by Turan Assylkhan on 01.02.2021.
+//  Copyright © 2021 Turan Assylkhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
