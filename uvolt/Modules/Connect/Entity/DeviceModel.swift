c64eee437bbf812d0dab06b00749f691fa044cb8
Turan Assylkhan <turan@strongte.am>
2021-02-01 13:33:05 +0600

    feat: Device model

---
+++ uvolt/Modules/Connect/Entity/DeviceModel.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceModel.swift
+//  uvolt
+//
+//  Created by Turan Assylkhan on 01.02.2021.
+//  Copyright © 2021 Turan Assylkhan. All rights reserved.
+//
+
+
+struct DeviceModel {
+    let name: String
+    let uudid: String
+}
