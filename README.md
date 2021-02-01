eb6d696e40a42e2c28a92709a25554455c1d07d7
Turan Assylkhan <turan@strongte.am>
2021-02-01 08:31:43 +0600

    feat: init project

---
+++ README.md
@@ -0,0 +1,9 @@
+### Initial setup
+
+Project contains:
+
+- Cartfile to configure depdendencies.
+
+To apply them run following commands:
+
+- ` ./carthage_build update --no-use-binaries --platform iOS `
--- uvolt/PreloadViewController.swift
+++ uvolt/PreloadViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PreloadViewController.swift
 //  uvolt
 //
 //  Created by Turan Assylkhan on 01.02.2021.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PreloadViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
