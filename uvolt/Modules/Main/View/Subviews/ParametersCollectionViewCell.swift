a6e5457acefd4c56f93c27b517d5f5512ccbfb7d
Turan Assylkhan <turan@strongte.am>
2021-02-02 08:16:56 +0600

    feat: cell initt

---
+++ uvolt/Modules/Main/View/Subviews/ParametersCollectionViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  ParametersCollectionViewCell.swift
+//  uvolt
+//
+//  Created by Turan Assylkhan on 02.02.2021.
+//  Copyright © 2021 Turan Assylkhan. All rights reserved.
+//
+
+import UIKit
+
+class ParametersCollectionViewCell: UICollectionViewCell {
+    
+    // ------------------------------
+    // MARK: - UI components
+    // ------------------------------
+    
+    private let containerView = InformationRoundedView()
+    
+    // ------------------------------
+    // MARK: - Life cycle
+    // ------------------------------
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+//    override func prepareForReuse() {
+//        super.prepareForReuse()
+//        imageView.image = nil
+//    }
+    
+    // ------------------------------
+    // MARK: - Public methods
+    // ------------------------------
+    
+    
+    // ------------------------------
+    // MARK: - Private
+    // ------------------------------
+    
+    private func setupViews() {
+        backgroundColor = .clear
+        clipsToBounds = false
+        
+        setupViewsHierarchy()
+        setupConstraints()
+    }
+    
+    private func setupViewsHierarchy() {
+        [containerView].forEach(contentView.addSubview(_:))
+    }
+    
+    private func setupConstraints() {
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
