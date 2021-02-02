e4535dbef9120466a8952fec68dd2ea449f4c0bc
Turan Assylkhan <turan@strongte.am>
2021-02-02 08:48:05 +0600

    feat: resetButton configs

--- uvolt/Modules/Settings/View/SettingsViewController.swift
+++ uvolt/Modules/Settings/View/SettingsViewController.swift
@@ -13,13 +13,23 @@ class SettingsViewController: BaseViewController, SettingsViewInput {
     // ------------------------------
     // MARK: - Properties
     // ------------------------------
-
+    
+    private enum Constants {
+        static let resetButtonHeight = 60
+    }
     var output: SettingsViewOutput?
 
     // ------------------------------
     // MARK: - UI components
     // ------------------------------
 
+    private let resetButton = Button(
+        text: "RESET_ALL".localized(),
+        textStyle: .headingH5,
+        textColor: .white,
+        backgroundColor: Color.grayBackground,
+        cornerRadius: 14)
+    
     // ------------------------------
     // MARK: - Life cycle
     // ------------------------------
@@ -39,19 +49,28 @@ class SettingsViewController: BaseViewController, SettingsViewInput {
     // ------------------------------
     // MARK: - Private methods
     // ------------------------------
+    
+    @objc private func resetDidTap() {
+        print("Reset")
+    }
 
     private func setupViews() {
         view.backgroundColor = .black
+        resetButton.addTarget(self, action: #selector(resetDidTap), for: .touchUpInside)
 
         setupViewsHierarchy()
         setupConstraints()
     }
 
     private func setupViewsHierarchy() {
-
+        [resetButton].forEach(view.addSubview(_:))
     }
 
     private func setupConstraints() {
-        
+        resetButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(16)
+            $0.height.equalTo(Constants.resetButtonHeight)
+            $0.bottom.equalTo(-safeAreaBottomInset)
+        }
     }
 }
