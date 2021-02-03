561a56851464ab12b10247d4f13ca9d82008fa16
Turan Assylkhan <turan@strongte.am>
2021-02-03 21:29:12 +0600

    feat: dots

--- uvolt/Modules/Speed/View/Subviews/GaugeView.swift
+++ uvolt/Modules/Speed/View/Subviews/GaugeView.swift
@@ -61,7 +61,7 @@ class GaugeView: UIView {
     
     var startAngle: CGFloat = .pi * 3/4
     var endAngle: CGFloat = .pi/4 + .pi * 2
-    private let points = 1
+    private let points = 24
     
     var progressLayer: CAShapeLayer = {
         let layer = CAShapeLayer()
@@ -95,49 +95,46 @@ class GaugeView: UIView {
     // MARK: DRAWING
     
     override open func draw(_ rect: CGRect) {
-        
-        //        let center = CGPoint(x: bounds.width/2, y: bounds.height/2)
-        //        var radius: CGFloat = (min(bounds.width, bounds.height))/2 - ringThickness/2
-        
+        let smallThickness: CGFloat = 7
         // Progress Layer
         addLayer(progressLayer)
         drawBorderedLayer(&progressLayer, thickness: ringThickness)
         
         addLayer(borderLayer)
-        drawBorderedLayer(&borderLayer, thickness: 7)
-        addInnerIndicators()
+        drawBorderedLayer(&borderLayer, thickness: smallThickness)
+        addInnerIndicators(thickness: smallThickness)
         
         
         setupValueAndMeasurementViews()
         
-        //        let context = UIGraphicsGetCurrentContext()
-        //        let lineWidth: CGFloat = 6
-        //        let markers = 24
-        //        let gaugeColor = UIColor.white
-        //        radius = min(bounds.width, bounds.height) / 2
-        //        let startAngle: CGFloat = 0.75 * CGFloat(Double.pi)
-        //        let endAngle: CGFloat = 0.25 * CGFloat(Double.pi)
-        //        let outlinePath = UIBezierPath(arcCenter: center, radius: radius - (lineWidth / 2), startAngle: startAngle, endAngle: endAngle, clockwise: true)
-        //        outlinePath.lineWidth = lineWidth
-        //        gaugeColor.setStroke()
-        //        outlinePath.stroke()
-        //        context?.saveGState()
-        //        gaugeColor.setFill()
-        //        let angleDifference: CGFloat = 2 * .pi - startAngle + endAngle
-        //        let arcLengthPerMark: CGFloat = angleDifference / CGFloat(markers)
-        //        let markerWidth: CGFloat = lineWidth - 1
-        //        let markerSize: CGFloat = 13
-        //        let markerPath = UIBezierPath(rect: CGRect(x: -markerWidth / 2, y: 0, width: markerWidth, height: markerSize).integral)
-        //        context?.translateBy(x: rect.width / 2, y: rect.height / 2)
-        //        for i in 0...markers {
-        //            context?.saveGState()
-        //            let angle = arcLengthPerMark * CGFloat(i) + startAngle - .pi / 2
-        //            context?.rotate(by: angle)
-        //            context?.translateBy(x: 0, y: rect.height / 2 - markerSize)
-        //            markerPath.fill()
-        //            context?.rotate(by: CGFloat(Double.pi))
-        //            context?.restoreGState()
-        //        }
+//        let context = UIGraphicsGetCurrentContext()
+//        let lineWidth: CGFloat = 6
+//        let markers = 24
+//        let gaugeColor = UIColor.white
+//        let radius = min(bounds.width, bounds.height) / 2
+//        let startAngle: CGFloat = 0.75 * CGFloat(Double.pi)
+//        let endAngle: CGFloat = 0.25 * CGFloat(Double.pi)
+//        let outlinePath = UIBezierPath(arcCenter: center, radius: radius - (lineWidth / 2), startAngle: startAngle, endAngle: endAngle, clockwise: true)
+//        outlinePath.lineWidth = lineWidth
+//        gaugeColor.setStroke()
+//        outlinePath.stroke()
+//        context?.saveGState()
+//        gaugeColor.setFill()
+//        let angleDifference: CGFloat = 2 * .pi - startAngle + endAngle
+//        let arcLengthPerMark: CGFloat = angleDifference / CGFloat(markers)
+//        let markerWidth: CGFloat = lineWidth - 1
+//        let markerSize: CGFloat = 13
+//        let markerPath = UIBezierPath(rect: CGRect(x: -markerWidth / 2, y: 0, width: markerWidth, height: markerSize).integral)
+//        context?.translateBy(x: rect.width / 2, y: rect.height / 2)
+//        for i in 0...markers {
+//            context?.saveGState()
+//            let angle = arcLengthPerMark * CGFloat(i) + startAngle - .pi / 2
+//            context?.rotate(by: angle)
+//            context?.translateBy(x: 0, y: rect.height / 2 - markerSize)
+//            markerPath.fill()
+//            context?.rotate(by: CGFloat(Double.pi))
+//            context?.restoreGState()
+//        }
     }
     
     func strokeGauge() {
@@ -194,55 +191,30 @@ class GaugeView: UIView {
         subLayer.lineWidth = CGFloat(thickness)
     }
     
-    private func addInnerIndicators() {
-        let centerPoint = CGPoint(x: bounds.width/2, y: bounds.height/2)
-        for i in 1...points {
-            let indicatorLayer = CAShapeLayer()
-            indicatorLayer.frame = bounds
-            
-            let indicWidth = CGFloat(3)
-            let indicLength = CGFloat(8)
-            
-            let startValue = (frame.width / 3) * 0.95
-            let endValue = startValue + indicLength
-            let baseAngle = degreeToRadian(calculateDegree(for: CGFloat(i)))
-            
-            let startPoint = CGPoint(x: cos(-baseAngle) * startValue + centerPoint.x,
-                                     y: sin(-baseAngle) * startValue + centerPoint.y)
-            let endPoint = CGPoint(x: cos(-baseAngle) * endValue+centerPoint.x,
-                                   y: sin(-baseAngle) * endValue + centerPoint.y)
-            
-            let indicatorPath = UIBezierPath()
-            indicatorPath.move(to: startPoint)
-            indicatorPath.addLine(to: endPoint)
-            
-            indicatorLayer.path = indicatorPath.cgPath
-            indicatorLayer.fillColor = UIColor.clear.cgColor
-            indicatorLayer.strokeColor = UIColor.white.cgColor
-            indicatorLayer.lineWidth = indicWidth
-            
-            layer.addSublayer(indicatorLayer)
-        }
+    private func addInnerIndicators(thickness: CGFloat) {
+        let radius = min(bounds.width, bounds.height) / 2 - thickness
+        let center = CGPoint(x: bounds.width/2, y: bounds.height/2)
+        let circlePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        shapeLayer.position = CGPoint(x: bounds.midX - center.x, y: bounds.midY - center.y )
+        //change the fill color
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        //you can change the stroke color
+        shapeLayer.strokeColor = UIColor.white.cgColor
+        //you can change the line width
+        shapeLayer.lineWidth = 10
+        let one : NSNumber = 3
+        let two : NSNumber = 11
+        shapeLayer.lineDashPattern = [one, two]
+        shapeLayer.lineCap = CAShapeLayerLineCap.butt
+        
+        layer.addSublayer(shapeLayer)
     }
     
     private func addLayer(_ subLayer: CAShapeLayer) {
         if subLayer.superlayer == nil {
             layer.addSublayer(subLayer)
         }
-    }
-    
-    private func calculateDegree(for point: CGFloat) -> CGFloat {
-        let floatPoints: CGFloat = CGFloat(points)
-        if point == 1 {
-            return startAngle
-        } else if point == floatPoints {
-            return endAngle
-        } else {
-            return startAngle - ((360 - (endAngle - startAngle)) / CGFloat(floatPoints)) * point
-        }
-    }
-    
-    private func degreeToRadian(_ degree: CGFloat) -> CGFloat {
-        return CGFloat(degree * CGFloat(.pi / 90.0))
     }
 }
